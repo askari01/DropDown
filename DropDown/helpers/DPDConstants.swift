59b121f2df23fb18a60e056653521852e7d35bbf
Erdem Inan <erdem.inan@aproplan.com>
2019-05-23 15:40:13 +0200

    Adapt DropDown to accept sections

--- DropDown/helpers/DPDConstants.swift
+++ DropDown/helpers/DPDConstants.swift
@@ -19,6 +19,7 @@ internal struct DPDConstant {
 	internal struct ReusableIdentifier {
 
 		static let DropDownCell = "DropDownCell"
+        static let DropDownSectionHeader = "DropDownSectionHeader"
 
 	}
 
@@ -27,12 +28,14 @@ internal struct DPDConstant {
 		static let TextColor = UIColor.black
         static let SelectedTextColor = UIColor.black
 		static let TextFont = UIFont.systemFont(ofSize: 15)
+        static let SectionTitleTextFont = UIFont.systemFont(ofSize: 13)
 		static let BackgroundColor = UIColor(white: 0.94, alpha: 1)
 		static let SelectionBackgroundColor = UIColor(white: 0.89, alpha: 1)
 		static let SeparatorColor = UIColor.clear
 		static let CornerRadius: CGFloat = 2
         static let BorderWidth: CGFloat = 0
 		static let RowHeight: CGFloat = 44
+        static let SectionHeaderHeight: CGFloat = 34
 		static let HeightPadding: CGFloat = 20
 
 		struct Shadow {
---
+++ DropDown/src/DropDownSectionHeader.swift
@@ -0,0 +1,35 @@
+//
+//  DropDownSectionHeader.swift
+//  DropDown
+//
+//  Created by Erdem Inan on 23/05/2019.
+//  Copyright © 2019 Kevin Hirsch. All rights reserved.
+//
+
+import Foundation
+
+class DropDownSectionHeader: UITableViewHeaderFooterView {
+    
+    //MARK: - Properties
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+    //MARK: - UIView's
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setup()
+    }
+    
+}
+
+//MARK: - Setup
+
+extension DropDownSectionHeader {
+    
+    private func setup() {
+        backgroundColor = .clear
+    }
+    
+}
